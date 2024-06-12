c6f59b79c111bfa13a499758eeaab1d7ee419257
Laura Sliwa <l.sliwa@gmx.de>
2024-06-12 12:49:57 +0200

    Add color to the theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { initialColors } from "./lib/colors";
 import Color from "./Components/Color/Color";
 import "./App.css";
+import ColorForm from "../src/Components/ColorForm/ColorForm.jsx";
 
 function App() {
   return (
     <>
       <h1>Theme Creator</h1>
-
+      <ColorForm />
       {initialColors.map((color) => {
         return <Color key={color.id} color={color} />;
       })}
---
+++ src/Components/ColorForm/ColorForm.jsx
@@ -0,0 +1,69 @@
+//Acceptance Criteria
+
+// [] Users can input a role, hex value, and contrast text via a form to add a new color to
+// the theme.
+// [] The form should be prefilled with default values to guide user input.
+// [] Upon submission, the new color is added to the top of the colors and is displayed on
+// a color card to confirm addition.
+// [] The inputs for hex and contrastText should include both color and text input types
+// for easy and accurate color selection.
+
+// Tasks
+
+// [] Implement a ColorForm component that allows users to submit new colors to the theme.
+// [] Use a package to generate unique id's like nanoid or ui
+// [] Develop a ColorInput component to handle synchronized text and color inputs,
+// ensuring that they reflect the same value. ( Controlled Inputs )
+
+export default function ColorForm({
+  onSubmitColor,
+  initialData = { role: "some color", hex: "#123456", contrastText: "#ffffff" },
+}) {
+  function handleSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData);
+    onSubmitColor(data);
+  }
+
+  return (
+    <form className="color-form" onSubmit={handleSubmit}>
+      <label htmlFor="role">
+        Role
+        <br />
+        <input
+          type="text"
+          id="role"
+          name="role"
+          defaultValue={initialData.role}
+        />
+      </label>
+      <br />
+      <label htmlFor="hex">
+        Hex
+        <br />
+        <input type="text" id="hex" name="hex" defaultValue={initialData.hex} />
+        <input
+          type="color"
+          id="hex"
+          name="hex"
+          defaultValue={initialData.hex}
+        />
+      </label>
+      <br />
+      <label htmlFor="contrastText">
+        Contrast Text
+        <br />
+        <input
+          type="text"
+          id="contrastText"
+          name="contrastText"
+          defaultValue={initialData.contrastText}
+        />
+        <input type="color" id="contrastText" name="contrastText" />
+      </label>
+      <br />
+      <button type="submit">ADD COLOR</button>
+    </form>
+  );
+}
