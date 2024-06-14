8694e1006ac13c46031eb77766343140758f8a98
Laura Sliwa <l.sliwa@gmx.de>
2024-06-14 12:03:06 +0200

    Start work on delete color issue

--- src/Components/Color/Color.jsx
+++ src/Components/Color/Color.jsx
@@ -1,6 +1,21 @@
+import { useState } from "react";
 import "./Color.css";
 
 export default function Color({ color }) {
+  const [isConfirming, setIsConfirming] = useState(false);
+
+  function handleDelete() {
+    setIsConfirming(true);
+  }
+
+  function cancelDelete() {
+    setIsConfirming(false);
+  }
+
+  function confirmDelete() {
+    onDeleteColor(color.id);
+  }
+
   return (
     <div
       className="color-card"
@@ -12,6 +27,19 @@ export default function Color({ color }) {
       <h3 className="color-card-headline">{color.hex}</h3>
       <h4>{color.role}</h4>
       <p>contrast: {color.contrastText}</p>
+      {isConfirming ? (
+        <>
+          <p className="color-card-highlight">Really delete?</p>
+          <button className="color-delete" onClick={cancelDelete}>
+            Cancel
+          </button>
+          <button className="color-delete" onClick={confirmDelete}>
+            Delete
+          </button>
+        </>
+      ) : (
+        <button onClick={handleDelete}>Delete</button>
+      )}
     </div>
   );
 }
