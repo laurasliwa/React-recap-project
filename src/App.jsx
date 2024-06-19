b376890e1c8690b22a4f2067a067ac26e3d1c51d
Laura Sliwa <l.sliwa@gmx.de>
2024-06-19 10:34:43 +0200

    Finish delete color button

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
   function handleDeleteColor(id) {
     console.log("Ich bin die Funktion in der App.js", id);
     // state updaten https://github.com/neuefische/hh-web-24-3/blob/main/sessions/react-state-3/react-state-3.md#removing-from-an-array
-    setColors(colors.filter((color) => color !== id));
+    setColors(colors.filter((color) => color.id !== id));
   }
 
   return (
